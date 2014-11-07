d4ae75e1c888c96268b278eac541b61e81157c8c
unknown <shfeng1@697-334-Theoden.sf.k12.internal>
2014-11-07 11:00:23 -0800

    submitting assignment

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip soga = new SpaceShip();
 Star [] field;
 public void setup() 
@@ -32,11 +48,11 @@ public void keyPressed()
   }
   if(key == 'w')
   {
-    soga.accelerate(.3);
+    soga.accelerate(.3f);
   }
   if(key == 's')
   {
-    soga.accelerate(-.3);
+    soga.accelerate(-.3f);
   }
   if(key == ' ')
   {
@@ -180,3 +196,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
